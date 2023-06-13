b85985fbcb312af640637ccf8136a2a0de624ef5
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-13 15:55:05 +0200

    addedc onsole log inst first email + invoked

--- vue.js
+++ vue.js
@@ -13,11 +13,11 @@ createApp({
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then(function (response) {
                     const result = response.data;
-                    
+                    console.log(result.data.response)
                 });
         }
     },
     created () {
-
+        this.randomGenEmail ()
     }
 }).mount('#app')
