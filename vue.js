bb29a33445c473a040253cddf7e5cab770ff5ef6
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-13 15:59:37 +0200

    updated console log instruction

--- vue.js
+++ vue.js
@@ -13,7 +13,7 @@ createApp({
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then(function (response) {
                     const result = response.data;
-                    console.log(result.data.response)
+                    console.log(result.response)
                 });
         }
     },
